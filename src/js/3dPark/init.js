6dba06166f7613b27dfa71250c81e0754863de28
chenht <chenht@kongtrolink.com>
2017-02-18 16:09:13 +0800

    [Add]1.some features of threejs files.

--- src/js/3dPark/init.js
+++ src/js/3dPark/init.js
@@ -11,17 +11,51 @@ var Scene = function () {
             matrix:{
                 width: 10,
                 height:10
+            },
+            brushSize:{
+                width:1,
+                height:1
             }
         },
         system:{
             matrix:{
                 size:5,
-                color:"#ff00ff"
+
+            },
+            color:{
+                floor:"#ffffff",
+                safe:"#00ff00",
+                warning:"#ff0000"
             }
         }
     };
     this.meshs = {
         floors:[],
+    };
+
+    this.model = {
+        cur:{
+
+        },
+        temp:{
+            geo:new THREE.CubeGeometry(8,8,8),
+            mat:new THREE.MeshBasicMaterial({color:new THREE.Color("#123456")})
+        }
+    };
+
+    this.recover = {
+        list:[],
+        allColor(){
+            this.list.forEach(function (i) {
+                i.material.color.set(i.recoverColor); 
+                i.recoverColor = null;
+            });
+            this.list = [];
+        },
+        pushColor(t){
+            this.list.push(t);
+            t.recoverColor = t.material.color.getHex();
+        }
     };
 
     this.handler = {
@@ -72,22 +106,106 @@ var Scene = function () {
         changeStateTo(s){
             var _this_ = this;
             this.curState = s;
+            this.fn.clearStat();
             switch(s){
                 case "floorHover": 
+                    scope.raycaster.targetSet("floor");
                     scope.canvas.onmousemove = function () {
                        _this_.fn.floorHover(); 
                     };
                     break;
+                case "modelHover": 
+                    scope.raycaster.targetSet("floor");
+                    scope.model.cur.mesh = new THREE.Mesh(scope.model.cur.geo,scope.model.cur.mat);
+                    scope.model.cur.mesh.position.y = 99999;
+                    scope.model.cur.mesh.material.transparent = true;
+                    scope.model.cur.mesh.material.opacity = 0.5;
+                    scope.scene.add(scope.model.cur.mesh);
+                    scope.canvas.onmousemove = function () {
+                       _this_.fn.floorHover(); 
+                       _this_.fn.modelHover(); 
+                    };
+                    break;
                 default:
+                    scope.raycaster.targetSet("");
                     scope.canvas.onmousemove = null;
                     scope.canvas.onmousedown = null;
                     scope.canvas.onmouseup = null;
             }
         },
         fn:{
+            clearStat(){
+                if(scope.model.cur.mesh){
+                    scope.scene.remove(scope.model.cur.mesh);
+                    scope.model.cur.mesh = null;
+                }
+            },
+            getSquare(x,z){
+                var flag = true;
+                var x0 = x - Math.ceil(scope.setting.user.brushSize.width/2) + 1;
+                var x1 = x + Math.floor(scope.setting.user.brushSize.width/2); 
+                var z0 = z - Math.ceil(scope.setting.user.brushSize.height/2) + 1;
+                var z1 = z + Math.floor(scope.setting.user.brushSize.height/2); 
+                if( x0 < 0 ){
+                    flag = false;
+                    x0 = 0;
+                }
+                if( x1 > scope.setting.user.matrix.width - 1){
+                    flag = false;
+                    x1 = scope.setting.user.matrix.width - 1; 
+                }
+                if( z0 < 0 ){
+                    flag = false;
+                    z0 = 0;
+                }
+                if( z1 > scope.setting.user.matrix.height - 1){
+                    flag = false;
+                    z1 = scope.setting.user.matrix.height - 1; 
+                }
+                var arr = [];
+                var counter = 0;
+                for(var i = 0 ;i < x1-x0+1;i++){
+                    for(var j = 0 ;j< z1-z0+1;j++,counter++){
+                        arr[counter] = scope.meshs.floors[x0+i][z0+j].mesh;
+                    }
+                }
+                return {
+                    valid:flag,
+                    arr:arr
+                };
+                
+            },
             floorHover(){
+                //recover
+                scope.recover.allColor();
+
                 if(scope.raycaster.intersect){
-                    scope.raycaster.intersect.object.material.color.set(0xff0000);
+                    var target = scope.raycaster.intersect.object;
+                    var result = this.getSquare(target.floor.coord.x,target.floor.coord.z);
+                    for(var i = 0;i<result.arr.length;i++){
+                        scope.recover.pushColor(result.arr[i]);
+                        if(result.valid){
+                            result.arr[i].material.color.set(scope.setting.system.color.safe);
+                        }
+                        else{
+                            result.arr[i].material.color.set(scope.setting.system.color.warning);
+                        }
+                    }
+                }
+            },
+            modelHover(){
+                scope.model.cur.mesh.position.y = 99999;
+                if(scope.raycaster.intersect){
+                    var target = scope.raycaster.intersect.object;
+                    var result = this.getSquare(target.floor.coord.x,target.floor.coord.z);
+                    var x0 = target.floor.coord.x - Math.ceil(scope.setting.user.brushSize.width/2) + 1;
+                    var x1 = x0 + parseInt(scope.setting.user.brushSize.width) - 1;
+                    var z0 = target.floor.coord.z - Math.ceil(scope.setting.user.brushSize.height/2) + 1;
+                    var z1 = z0 + parseInt(scope.setting.user.brushSize.height) - 1;
+                    scope.model.cur.mesh.position.set(
+                        (x0 + 1 + x1) / 2 * scope.setting.system.matrix.size,
+                        5,
+                        (z0 + 1 + z1) / 2 * scope.setting.system.matrix.size);
                 }
             }
         }
@@ -101,21 +219,21 @@ var Scene = function () {
             this.meshs.floors[i] = [];
             for(var j = 0; j < this.setting.user.matrix.height; j++){
                 var geo =  new THREE.PlaneBufferGeometry(this.setting.system.matrix.size,this.setting.system.matrix.size);
-                var mat = new THREE.MeshBasicMaterial({color:new THREE.Color(this.setting.system.matrix.color)});
+                var mat = new THREE.MeshBasicMaterial({color:new THREE.Color(this.setting.system.color.floor)});
                 var mesh = new THREE.Mesh(geo,mat);
                 mesh.position.x = i * this.setting.system.matrix.size + this.setting.system.matrix.size / 2;
                 mesh.position.z = j * this.setting.system.matrix.size + this.setting.system.matrix.size / 2;
                 mesh.rotation.x = -0.5*Math.PI;
-                mesh.targetType = ["floor"]
+                mesh.targetType = ["floor"];
                 this.scene.add(mesh);
-                this.meshs.floors[i].push({
+                mesh.floor = {
                     mesh:mesh,
                     coord:{
                         x:i,
                         z:j
                     }
-                });
-
+                };
+                this.meshs.floors[i].push(mesh.floor);
 
                 if(i===0 && j===0){
                     mesh.material.color.set(0xff0000);
@@ -175,6 +293,3 @@ var Scene = function () {
     };
 };
 
-var scene = new Scene();
-
-scene.init();
