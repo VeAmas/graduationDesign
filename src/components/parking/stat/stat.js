224f16bbaf606bd415057a21fb3148d65c205dc5
VeAmas <519132364@qq.com>
2017-05-01 19:36:48 +0800

    [Add]

--- src/components/parking/stat/stat.js
+++ src/components/parking/stat/stat.js
@@ -10,12 +10,12 @@ const parking_stat = {
 						<table class="table table-striped table-hover">
 					      <thead>
 					        <tr>
-					          <th>泊位编号</th>
-					          <th>泊位状态</th>
-					          <th>当前停泊车辆</th>
-					          <th>停泊线路</th>
-					          <th>更新时间</th>
-					          <th>操作</th>
+					          <th>停车场名称</th>
+					          <th>泊位数量</th>
+					          <th>占用数量</th>
+					          <th>泊位一览</th>	
+					          <th>车辆一览</th>				          
+					          <th>3D显示</th>
 					        </tr>
 					      </thead>
 					      <tbody>
@@ -23,16 +23,13 @@ const parking_stat = {
 					        	<td v-text="item.set.name"></td>
 					        	<td v-text="item.set.available ? '无车辆' : '有车辆'"></td>
 					        	<td v-text="item.vehicle.license"></td>
-					        	<td v-text="item.vehicle.route"></td>
-					        	<td v-text="item.set.lastRecordTime"></td>
-					        	<td class="operate-bar">
-									<a class = "operate" title="指派泊位" @click="parkingSetSpecify.toModal(item)">
-										<span class="glyphicon glyphicon-cog"></span>
-									</a>
-									<a class = "operate" title="删除泊位" @click="parkingSetDelete.toModal(item)">
-										<span class="glyphicon glyphicon-remove"></span>
-									</a>
+					        	<td>
+					          		<a @click='showAllSet.toModal()'>泊位一览</a>	
 					        	</td>
+					        	<td>
+					          		<a @click='showAllVehicle.toModal()'>车辆一览</a>		
+					        	</td>
+					        	<td v-text="item.set.lastRecordTime"></td>
 					        </tr>
 					      </tbody>
 				    	</table>
@@ -43,28 +40,20 @@ const parking_stat = {
 					</div>
 				</div>
 			</div>
-			<modal v-if = "parkingSetSpecify.isShow" id="vehicle_modify">
+			<modal v-if = "showAllSet.isShow" id="vehicle_delete">
 				<div class="modal-header">
-					<h4 class="modal-title">车辆修改</h4>
-				</div>
-				<div class="modal-body">
-					您将把该车位指派给&nbsp;<span class="high-light">车辆&nbsp;</span> 
-					<select  v-model="parkingSetSpecify.obj.specifyVehicle">
-						<option v-for='item in parkingSetSpecify.vehicleList' v-text='item.license' :value='item.license'/>
-					</select>
 				</div>
 				<div class="modal-footer">
-					<button class="btn ok" @click="parkingSetSpecify.execute()">确认</button>
-					<button class="btn cancle" @click="parkingSetSpecify.clear()">关闭</button>
+					<button class="btn ok" @click="showAllSet.clear()">确认</button>
+					<button class="btn cancle" @click="showAllSet.clear()">取消</button>
 				</div>
 			</modal>
-			<modal v-if = "parkingSetDelete.isShow" id="vehicle_delete">
+			<modal v-if = "showAllVehicle.isShow" id="vehicle_delete">
 				<div class="modal-header">
-					<h4 class="modal-title">是否删除编号为&nbsp;<span class="high-light" v-text="parkingSetDelete.obj.set.name"></span>&nbsp;的车位？</h4>
 				</div>
 				<div class="modal-footer">
-					<button class="btn ok" @click="parkingSetDelete.clear()">确认</button>
-					<button class="btn cancle" @click="parkingSetDelete.clear()">取消</button>
+					<button class="btn ok" @click="showAllVehicle.clear()">确认</button>
+					<button class="btn cancle" @click="showAllVehicle.clear()">取消</button>
 				</div>
 			</modal>
 		</section>
@@ -73,10 +62,10 @@ const parking_stat = {
 		return {
 			vehicleRoute:[],
 			parkingSetList:[],
-			parkingSetSpecify:{
+			showAllSet:{
 				isShow:false
 			},
-			parkingSetDelete:{
+			showAllVehicle:{
 				isShow:false
 			}
 		};
@@ -84,8 +73,8 @@ const parking_stat = {
 	methods:{
 	},
 	created(){
-		this.parkingSetSpecify.obj={};
-		this.parkingSetSpecify.toModal = function () {
+		this.showAllSet.obj={};
+		this.showAllSet.toModal = function () {
 			this.vehicleList = [{
 				route:"123",
 				license:"浙A·12345",
@@ -109,23 +98,23 @@ const parking_stat = {
 			}];
 			this.isShow = true;
 		};
-		this.parkingSetSpecify.clear = function () {
+		this.showAllSet.clear = function () {
 			this.isShow = false;
 		};
-		this.parkingSetSpecify.execute = function () {
+		this.showAllSet.execute = function () {
 			this.isShow = false;
 		};
 
-		this.parkingSetDelete.obj={};
-		this.parkingSetDelete.toModal = function (item) {
+		this.showAllVehicle.obj={};
+		this.showAllVehicle.toModal = function (item) {
 			this.obj = item;
 			this.isShow = true;
 		};
-		this.parkingSetDelete.execute = function () {
+		this.showAllVehicle.execute = function () {
 			this.obj = {};
 			this.isShow = false;
 		};
-		this.parkingSetDelete.clear = function () {
+		this.showAllVehicle.clear = function () {
 			this.obj = {};
 			this.isShow = false;
 		};
--- src/components/vehicle/assign/assign.js
+++ src/components/vehicle/assign/assign.js
@@ -17,7 +17,7 @@ const vehicle_assign = {
 					          <th>车牌号</th>
 					          <th>当前驾驶员</th>
 					          <th>当前状态</th>
-					          <th>更新时间</th>
+					          <th>下次出车时间</th>
 					          <th>操作</th>
 					        </tr>
 					      </thead>
@@ -27,16 +27,12 @@ const vehicle_assign = {
 					        	<td v-text="item.vehicle.license"></td>
 					        	<td v-text="item.driver.name"></td>
 					        	<td v-text="item.vehicle.curStat"></td>
-					        	<td v-text="item.vehicle.lastRecordTime"></td>
+					        	<td>
+									<a v-text="item.nextStart" title="出车" @click="modifyNextStart.toModal(item)"></a>
+					        	</td>
 					        	<td class="operate-bar">
-									<a class = "operate" title="查看" @click="vehicleCheck.toModal(item)">
-										<span class="glyphicon glyphicon-file"></span>
-									</a>
-									<a class = "operate" title="编辑" @click="vehicleModify.toModal(item)">
-										<span class="glyphicon glyphicon-cog"></span>
-									</a>
-									<a class = "operate" title="删除" @click="vehicleDelete.toModal(item)">
-										<span class="glyphicon glyphicon-remove"></span>
+									<a class = "operate" title="出车" @click="startOut.toModal(item)">
+										出车
 									</a>
 					        	</td>
 					        </tr>
@@ -48,125 +44,24 @@ const vehicle_assign = {
 						<div id="vehicle-stat-pagination"></div>
 					</div>
 				</div>
-			</div>
-			<modal v-if = "vehicleCheck.isShow" id="vehicle_check">
+			</div> 		
+			<modal v-if = "startOut.isShow" id="vehicle_delete">
 				<div class="modal-header">
-					<h4 class="modal-title">车辆信息</h4>
-				</div>
-				<div class="modal-body">
-					<div class="vehicle-info">
-						<div class="photo">
-							<img :src="vehicleCheck.obj.vehicle.photo" />
-						</div>
-						<div class="stat">
-							<table>
-								<tr><td>车牌号</td><td v-text="vehicleCheck.obj.vehicle.license"></td></tr>
-								<tr><td>公交线路</td><td v-text="vehicleCheck.obj.vehicle.route"></td></tr>
-								<tr><td>车辆型号</td><td v-text="vehicleCheck.obj.vehicle.model"></td></tr>
-								<tr><td>购买日期</td><td v-text="vehicleCheck.obj.vehicle.purchasedDate"></td></tr>
-								<tr><td>保养等级</td><td v-text="vehicleCheck.obj.vehicle.maintenance"></td></tr>
-								<tr><td>行驶公里数</td><td v-text="vehicleCheck.obj.vehicle.km"></td></tr>
-								<tr><td>上一次更新时间</td><td v-text="vehicleCheck.obj.vehicle.lastRecordTime"></td></tr>
-								<tr><td>当前状态</td><td v-text="vehicleCheck.obj.vehicle.curStat"></td></tr>
-							</table>
-						</div>
-					</div>
-					<div>当前驾驶员信息：</div>
-					<div class="driver-info">
-						<div class="photo">
-							<img :src="vehicleCheck.obj.driver.photo" />
-						</div>
-						<div class="stat">
-							<table>
-								<tr>
-									<td>姓名</td><td v-text="vehicleCheck.obj.driver.name"></td>
-									<td>年龄</td><td v-text="vehicleCheck.obj.driver.age"></td>
-									<td>性别</td><td v-text="vehicleCheck.obj.driver.gender"></td>
-								</tr>
-								<tr><td>出生日期</td><td colspan="5" v-text="vehicleCheck.obj.driver.birth"></td></tr>
-								<tr><td>身份证号码</td><td colspan="5" v-text="vehicleCheck.obj.driver.ID"></td></tr>
-								<tr><td>地址</td><td colspan="5" v-text="vehicleCheck.obj.driver.address"></td></tr>
-								<tr><td>联系电话</td><td colspan="5" v-text="vehicleCheck.obj.driver.cellPhone"></td></tr>
-								<tr><td>邮件</td><td colspan="5" v-text="vehicleCheck.obj.driver.email"></td></tr>
-								<tr><td>开始工作日期</td><td colspan="5" v-text="vehicleCheck.obj.driver.startDate"></td></tr>
-							</table>
-						</div>
-					</div>
+					<h4 class="modal-title">是否删除车牌号为&nbsp;<span class="high-light" v-text="startOut.obj.vehicle.license"></span>&nbsp;的车辆？</h4>
 				</div>
 				<div class="modal-footer">
-					<button class="btn cancle" @click="vehicleCheck.clear()">关闭</button>
+					<button class="btn ok" @click="startOut.clear()">确认</button>
+					<button class="btn cancle" @click="startOut.clear()">取消</button>
 				</div>
 			</modal>
-			<modal v-if = "vehicleModify.isShow" id="vehicle_modify">
+			<modal v-if = "modifyNextStart.isShow" id="vehicle_delete">
 				<div class="modal-header">
-					<h4 class="modal-title">车辆修改</h4>
-				</div>
-				<div class="modal-body">
-					<div class="vehicle-info">
-						<div class="photo">
-							<img :src="vehicleModify.obj.vehicle.photo" />
-						</div>
-						<div class="stat">
-							<table>
-								<tr>
-									<td>车牌号</td>
-									<td>
-										<select class = 'license' v-model="vehicleModify.obj.license.front">
-											<option v-for="p in common.province" :value="p" v-text="p"></option>
-										</select>
-										<input class = 'license end' type="text" v-model="vehicleModify.obj.license.end" maxlength="5"/>
-										<input class = 'license middle' type="text" v-model="vehicleModify.obj.license.middle" maxlength="1"/>
-									</td>
-								</tr>
-								<tr><td>公交线路</td>
-									<td>
-										<input type="text" v-model="vehicleModify.obj.vehicle.route"/>
-									</td>
-								</tr>
-								<tr><td>车辆型号</td>
-									<td>
-										<input type="text" v-model="vehicleModify.obj.vehicle.model"/>
-									</td>
-								</tr>
-								<tr><td>购买日期</td>
-									<td>
-										<datetime-picker class='purchasedDate' :data="vehicleModify.obj.vehicle.purchasedDate"></datetime-picker>
-									</td>
-								</tr>
-								<tr><td>保养等级</td>
-									<td>
-										<select v-model="vehicleModify.obj.vehicle.maintenance">
-											<option v-for="p in common.maintenanceLevel" :value="p" v-text="p"></option>
-										</select>
-									</td>
-								</tr>
-								<tr><td>行驶公里数</td>
-									<td>
-										<input type="text" v-model="vehicleModify.obj.vehicle.km"/>
-									</td>
-								</tr>
-								<tr><td>当前状态</td>
-									<td>
-										<select v-model="vehicleModify.obj.vehicle.curStat">
-											<option v-for="p in common.vehicleStat" :value="p" v-text="p"></option>
-										</select>
-									</td>
-								</tr>
-							</table>
-						</div>
-					</div>
+					<h4 class="modal-title">修改车牌号为 &nbsp;<span class="high-light" v-text="modifyNextStart.obj.vehicle.license"></span>&nbsp;的车辆的下次发车时间至：</h4></br>
+					<datetime-picker class='modifyNextStartDatetimePicker' :data="modifyNextStart.obj.nextStart"></datetime-picker>
 				</div>
 				<div class="modal-footer">
-					<button class="btn cancle" @click="vehicleModify.clear()">关闭</button>
-				</div>
-			</modal>
-			<modal v-if = "vehicleDelete.isShow" id="vehicle_delete">
-				<div class="modal-header">
-					<h4 class="modal-title">是否删除车牌号为&nbsp;<span class="high-light" v-text="vehicleDelete.obj.vehicle.license"></span>&nbsp;的车辆？</h4>
-				</div>
-				<div class="modal-footer">
-					<button class="btn ok" @click="vehicleDelete.clear()">确认</button>
-					<button class="btn cancle" @click="vehicleDelete.clear()">取消</button>
+					<button class="btn ok" @click="modifyNextStart.clear()">确认</button>
+					<button class="btn cancle" @click="modifyNextStart.clear()">取消</button>
 				</div>
 			</modal>
 		</section>
@@ -175,13 +70,10 @@ const vehicle_assign = {
 		return {
 			vehicleRoute:[],
 			vehicleList:[],
-			vehicleCheck:{
+			startOut:{
 				isShow:false
-			},
-			vehicleModify:{
-				isShow:false
-			},
-			vehicleDelete:{
+			},			
+			modifyNextStart:{
 				isShow:false
 			}
 		};
@@ -193,40 +85,30 @@ const vehicle_assign = {
 			console.log(selected);
 		});
 
-		this.vehicleCheck.obj={};
-		this.vehicleCheck.toModal = function (item) {
+		this.startOut.obj={};
+		this.startOut.toModal = function (item) {
 			this.obj = item;
 			this.isShow = true;
 		};
-		this.vehicleCheck.clear = function () {
+		this.startOut.clear = function () {
+			this.obj = {};
+			this.isShow = false;
+		};
+		this.startOut.execute = function () {
 			this.obj = {};
 			this.isShow = false;
 		};
 
-		this.vehicleModify.obj={};
-		this.vehicleModify.toModal = function (item) {
-			$.extend(this.obj ,item);
-			this.obj.license={
-				front:item.vehicle.license.substr(0,1),
-				middle:item.vehicle.license.substr(1,1),
-				end:item.vehicle.license.substr(3)
-			};
-			this.isShow = true;
-		};
-		this.vehicleModify.clear = function () {
-			this.isShow = false;
-		};
-
-		this.vehicleDelete.obj={};
-		this.vehicleDelete.toModal = function (item) {
+		this.modifyNextStart.obj={};
+		this.modifyNextStart.toModal = function (item) {
 			this.obj = item;
 			this.isShow = true;
 		};
-		this.vehicleDelete.execute = function () {
+		this.modifyNextStart.clear = function () {
 			this.obj = {};
 			this.isShow = false;
 		};
-		this.vehicleDelete.clear = function () {
+		this.modifyNextStart.execute = function () {
 			this.obj = {};
 			this.isShow = false;
 		};
@@ -303,9 +185,10 @@ const vehicle_assign = {
 					email:"",
 					startDate:""
 				},
+				nextStart: '2016.2.2'
 
 			}];
-			for(var i = 0;i<100;i++){
+			for(var i = 0;i<30;i++){
 				_this_.vehicleList.push(_this_.vehicleList[0]);
 			}
 		},1000);
